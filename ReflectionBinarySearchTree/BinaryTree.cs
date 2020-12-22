bbe304acda6b826955bb073cce59ff22be9accd4
Hakop Zarikyan <Hakop.Zarikyan@GMR.local>
2020-12-21 18:16:04 -0800

    Add project files.

---
+++ ReflectionBinarySearchTree/BinaryTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+namespace ReflectionBinarySearchTree
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public Node()
+        {}
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+
+    public class BinaryTree<T>
+    {
+        public Node<T> Head;
+        public int Count;
+
+        public Func<T, T, int> comparer;
+
+        public BinaryTree()
+        {
+            Head = new Node<T>();
+            Count = 0;
+            var type = typeof(T);
+            if(!GetComparer(type))
+            {
+                if(!GetComparer(type.BaseType))
+                {
+                    throw new Exception("No Comparable variables");
+                }
+            }
+            throw new NotImplementedException("Not done");
+            
+
+        }
+        public void Add(T value)
+        {
+            if(Count == 0)
+            {
+                Head = new Node<T>(value);
+            }
+            else
+            {
+                
+            }
+        }
+        private Node<T> add(T value)
+        {
+            return default; 
+        }
+        bool GetComparer(Type type)
+        {
+            var fields = type.GetFields().Where(x => x.CustomAttributes.Count() > 0);
+            var comp = fields.Where(x => Attribute.IsDefined(x, typeof(CompareBy)));
+            if (comp.Count() > 1)
+            {
+                throw new Exception("Too many Compare By Attributes in One Class");
+            }
+            if (comp.Count() == 0)
+            {
+                return false;
+            }
+            var variableToCompare = comp.First();
+            if (variableToCompare.FieldType is IComparable<T>)
+            {
+                return true;
+            }
+            else
+            {
+                throw new NotSupportedException("Does not support other than IComparable");
+            }
+        }
+    }
+}
---
+++ ReflectionBinarySearchTree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ReflectionBinarySearchTree
+{
+    class CompareBy : Attribute
+    {
+    }
+    class Animal
+    {
+        [CompareBy]
+        public string Name { get; set; }
+
+        public bool IsExtinct { get; set; }
+    }
+
+    class Giraffe : Animal
+    {
+        [CompareBy]
+        public int NeckLength { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Binary Tree with Add
+            //Figure out if the type has an IComparable or function for Comparable
+            //***Use Reflection***
+            Console.WriteLine("Hello World!");
+
+            var bst = new BinaryTree<Giraffe>();
+        }
+    }
+}
